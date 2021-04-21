2543933b695de11c99150e9df9fb76692ae70bcb
jurafish <jurafish@163.com>
2021-04-21 09:24:34 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -xe
-shopt -s globstar
 cd "$(dirname "$0")"
 
 for script in scripts.d/**/*.sh; do
+    echo '${script}'
+    echo '$script'
     if [ "${script}" == "scripts.d/45-vulkan.sh" ] 
     then
       echo 'vulkan ...........................' 
