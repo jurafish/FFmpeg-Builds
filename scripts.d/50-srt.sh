b66e35786f0bbebba8e5fcaf85e90c49e9c488bf
jurafish <jurafish@163.com>
2021-04-21 15:55:10 +0800

    Update 50-srt.sh

--- scripts.d/50-srt.sh
+++ scripts.d/50-srt.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 SRT_REPO="https://github.com/Haivision/srt.git"
-SRT_COMMIT="60ae6e56014b5ee48c8e25eda4d7fcc2e28f79cc"
+#SRT_COMMIT="60ae6e56014b5ee48c8e25eda4d7fcc2e28f79cc"
+SRT_COMMIT="9e52606fae3b32818b5b734d45d3358f4a95cf3f"
 
 ffbuild_enabled() {
     return 0
