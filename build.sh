39f434a255d2f0ed8b9341a7c4827cc6163a5747
jurafish <jurafish@163.com>
2021-04-21 11:25:59 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -33,6 +33,10 @@ for script in scripts.d/**/*.sh; do
     then
       continue
     fi
+    if [ "${script}" == "scripts.d/50-rav1e.sh" ]
+    then
+      continue
+    fi
     FF_CONFIGURE+=" $(get_output $script configure)"
     FF_CFLAGS+=" $(get_output $script cflags)"
     FF_CXXFLAGS+=" $(get_output $script cxxflags)"
